a20f8f5a7c091ee5103a8f1b6dfdda1a41b4cfff
T-Uchiyama <t_uchiyama@funteam.co.jp>
2017-02-01 16:39:26 +0900

    Tag用管理画面作成

--- app/Controller/TagsController.php
+++ app/Controller/TagsController.php
@@ -1,7 +1,74 @@
 <?php
-    class TagsController extends AppController {
-        public function index() {
+    class TagsController extends AppController
+    {
+        public function index()
+        {
             $tags = $this->Tag->find('all');
-            $this->set('tags', $tags);       
+            $this->set('tags', $tags);
+        }
+
+        public function add()
+        {
+            if ($this->request->is('post'))
+            {
+                $this->Tag->create();
+
+                if ($this->Tag->save($this->request->data))
+                {
+                    $this->Flash->success(__('The tag has been saved'));
+                    return $this->redirect(array('action' => 'index'));
+                }
+                $this->Flash->error(__('The tag could not be saved. Please, try again.'));
+            }
+        }
+
+        public function edit($id = null)
+        {
+            if (!$id) {
+    	        throw new NotfoundException(__('Invalid Tag'));
+    	    }
+
+            $tags = $this->Tag->findById($id);
+
+    	    if (!$tags) {
+      	        throw new NotFoundException(__('Invalid Tag'));
+    	    }
+
+            $this->set('tags', $tags);
+
+
+    	    if ($this->request->is(array('post', 'put')))
+            {
+                $this->Tag->id = $id;
+    	        if ($this->Tag->saveAll($this->request->data)) {
+    	            $this->Flash->success(__('Your Tag has been updated.'));
+    	            return $this->redirect(array('action' => 'index'));
+    	            }
+    	        $this->Flash->error(__('Unable to update your Tag.'));
+    	    }
+
+    	    if (!$this->request->data)
+            {
+    	        $this->request->data = $tags;
+    	    }
+        }
+
+        public function delete($id)
+        {
+    	    if ($this->request->is('get'))
+            {
+    	        throw new MethodNotAllowedException();
+    	    }
+
+    	    if ($this->Tag->delete($id))
+            {
+    	        $this->Flash->success(
+    		    __('The tag with id: %s has been deleted.', h($id)));
+    	    } else {
+    	        $this->Flash->error(
+    		    __('The tag with id: %s could not be deleted.', h($id)));
+        	}
+
+    	    return $this->redirect(array('action' => 'index'));
         }
     }
--- app/View/Tags/index.ctp
+++ app/View/Tags/index.ctp
@@ -1,10 +1,16 @@
 <!-- File: /app/View/tags/index.ctp -->
 
 <h1>Tags List</h1>
+<?php
+    echo $this->Html->link(
+        'Add Tag',
+        array ('controller' => 'tags', 'action' => 'add'));
+ ?>
 
 <table>
     <tr>
     <th>Id</th>
+    <th>Action</th>
     <th>Tagname</th>
     </tr>
 
@@ -19,7 +25,23 @@
         </td>
 
         <td>
-            <?php 
+             <?php
+                echo $this->Form->postLink(
+    		    'Delete',
+                array('action' => 'delete', $tag['Tag']['id']),
+                array('confirm' => 'Are you sure?')
+    	        );
+    	     ?>
+
+    	    <?php
+                echo $this->Html->link(
+    		    'Edit',
+    		    array('action' => 'edit', $tag['Tag']['id']));
+    	    ?>
+    	</td>
+
+        <td>
+            <?php
                 echo $tag['Tag']['title'];
             ?>
         </td>
