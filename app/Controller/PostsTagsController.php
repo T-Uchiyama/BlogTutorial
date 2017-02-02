41642923759432e9577ed700b4208af4c361b116
T-Uchiyama <t_uchiyama@funteam.co.jp>
2017-02-02 15:57:04 +0900

    ミスが発覚し修正

--- app/Controller/PostsTagsController.php
+++ app/Controller/PostsTagsController.php
@@ -2,6 +2,6 @@
     class PostsTagsController extends AppController {
         public function index() {
             $posts_tags = $this->PostsTag->find('all');
-            this->set('posts_tags', $posts_tags);
+            $this->set('posts_tags', $posts_tags);
          }
     }
