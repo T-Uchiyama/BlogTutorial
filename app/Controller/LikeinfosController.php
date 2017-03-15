60fbb79bfb9044d757be639b5a3bab1819702368
T-Uchiyama <t_uchiyama@funteam.co.jp>
2017-03-15 17:29:53 +0900

    いいねのキャンセル機能作成

--- app/Controller/LikeinfosController.php
+++ app/Controller/LikeinfosController.php
@@ -33,7 +33,34 @@
 
         public function delete()
         {
-            // TODO これから実装
+            $this->autoRender = false;
+            $postId = $this->request->data['post_id'];
+            $userName = $this->request->data['userName'];
+
+            // ユーザーIDとLikeInfoDBのIDをリクエストデータを使用し抽出
+            $userDataList = $this->Likeinfo->Post->User->find('list', array(
+                    'conditions' => array(
+                        'username' => $userName,
+                    ),
+                    'fields' => array('username', 'id'),
+                )
+            );
+
+            $likeDataList = $this->Likeinfo->find('list', array(
+                    'conditions' => array(
+                        'post_id' => $postId,
+                        'user_id' => $userDataList[$userName],
+                    ),
+                    'fields' => array('post_id', 'id')
+                )
+            );
+
+            if ($this->Likeinfo->delete($likeDataList[$postId]))
+            {
+                return json_encode('いいねをキャンセルしました。');
+            } else {
+                exit;
+            }
         }
     }
 ?>
